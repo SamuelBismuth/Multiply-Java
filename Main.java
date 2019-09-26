93b0cbe62e40983a7fdd30652c21a294ef5c600a
Samuel <sam101@hotmail.fr>
2019-09-26 14:52:05 +0200

    minor

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int myInt = scanner.nextInt();
+	scanner.close();
+	System.out.println(myInt * 2);
+    }
+
+}
+
